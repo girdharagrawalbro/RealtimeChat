55e27017eb6d8a9acad5300ed49606296c6cf396
Girdhar Agrawal <girdharagrawalbro@gmail.com>
2024-09-06 13:23:11 +0530

    second commit

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://realtimechat-ky13.onrender.com');
 const form = document.getElementById('send-container')
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector('.messagebox');
