4da360defaf7d3e4e80603b81337c5cfad1943b6
Girdhar Agrawal <girdharagrawalbro@gmail.com>
2024-09-06 13:10:40 +0530

    first commit

---
+++ js/client.js
@@ -0,0 +1,39 @@
+const socket = io('http://localhost:8000');
+const form = document.getElementById('send-container')
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.messagebox');
+var audio = new Audio('audio.mp3');
+
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position = 'text-start')
+    {
+        audio.play();
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'text-end')
+    socket.emit('send', message);
+    messageInput.value = "";
+})
+
+const data = prompt("Enter your name to join");
+socket.emit('user-joined', data);
+
+socket.on('user-joined', name => {
+    append(`${name} has joined the chat`, 'text-start');
+});
+
+socket.on('receive', data => {
+    append(`${data.user}: ${data.message}`, 'text-start');
+});
+
+socket.on('user-left', name => {
+    append(`${name} left the chat`, 'text-start');
+});
---
+++ nodeServer/index.js
@@ -0,0 +1,27 @@
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: 'http://127.0.0.1:5500',  // Allow all origins, or replace with the specific origin you want to allow
+        methods: ['GET', 'POST']
+    }
+});
+
+const users = {};
+
+io.on('connection', socket => {
+    // Event listener for when a user joins
+    socket.on('user-joined', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    // Event listener for sending messages
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, user: users[socket.id] });
+    });
+
+    // Optional: Event listener for disconnect
+    socket.on('disconnect', name => {
+        socket.broadcast.emit('user-left', users[socket.id]);
+        delete users[socket.id];
+    });
+});
