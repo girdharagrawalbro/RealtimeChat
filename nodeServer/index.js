aacc279c5003a62f542d3208c695c403d2d17af1
Girdhar Agrawal <girdharagrawalbro@gmail.com>
2024-09-06 13:46:23 +0530

    4 commit

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,6 +1,6 @@
 const io = require('socket.io')(8000, {
     cors: {
-        origin: 'https://chatapp-girdhar.netlify.app/',  // Allow all origins, or replace with the specific origin you want to allow
+        origin: 'https://chatapp-girdhar.netlify.app',  // Allow all origins, or replace with the specific origin you want to allow
         methods: ['GET', 'POST']
     }
 });
